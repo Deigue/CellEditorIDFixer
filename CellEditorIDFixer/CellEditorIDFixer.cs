9eddd4e173348cfee207ee4c342568c6029c09d9
Deigue <deigue@hotmail.com>
2020-10-25 22:29:09 -0400

    Cleaned up comment, adds worldspace cells support.

--- CellEditorIDFixer/CellEditorIDFixer.cs
+++ CellEditorIDFixer/CellEditorIDFixer.cs
@@ -20,8 +20,6 @@ namespace CellEditorIDFixer
                 patcher: RunPatch,
                 new UserPreferences()
                 {
-                    //AddImplicitMasters = false,
-                    //IncludeDisabledMods = true,
                     ActionsForEmptyArgs = new RunDefaultPatcher
                     {
                         
@@ -41,77 +39,24 @@ namespace CellEditorIDFixer
             var worldContexts = state.LoadOrder.PriorityOrder.Worldspace().WinningContextOverrides();
             var cellWorldspaceMap = new Dictionary<FormKey, ModContext<ISkyrimMod, IWorldspace, IWorldspaceGetter>>();
 
+            // Build Map to provide path to the winning worldspace context.
             state.LoadOrder.PriorityOrder
                 .ForEach(mod =>
                     {
                         mod.Mod?.Worldspaces.RecordCache.Items
                             .ForEach(world =>
                                 {
-                                    var context = worldContexts
-                                        .Where(wc => wc.Record.FormKey.ID == world.FormKey.ID)
+                                    var winningContext = worldContexts
+                                        .Where(wc => wc.Record.FormKey.Equals(world.FormKey))
                                         .First();
-                                    /*
-                                    context = new ModContext<ISkyrimMod, IWorldspace, IWorldspaceGetter>(
-                                            modKey: mod.ModKey,
-                                            record: world,
-                                            getter: (m, r) => m.Worldspaces.GetOrAddAsOverride(r)
-                                    );
-                                    */
+
                                     world.EnumerateMajorRecords<ICellGetter>()
                                         .Select(c => c.FormKey)
-                                        .ForEach(cellKey => cellWorldspaceMap.TryAdd(cellKey, context));
+                                        .ForEach(cellKey => cellWorldspaceMap.TryAdd(cellKey, winningContext));
                                 }
                             );
                     }
                 );
-
-
-            // ATTEMPT 1: Attempt to create HashMap of desired values <CELL FormKey, Worldspace>...
-            // Tuple.Create(w, w.EnumerateMajorRecords<ICellGetter>()))
-            //   .SelectMany(t => t.Item2)
-            // .Select(rec => Tuple.Create(rec.FormKey, w))
-            /*
-            Select(x => Tuple.Create(x, x.Item2.)
-            SelectMany(x => x.EnumerateMajorRecords<ICellGetter>())
-
-            .Select(x =>
-            {
-                FormKey = x.FormKey;
-                IWorldspaceGetter = x.p
-            })
-            .ToDictionary<FormKey, IWorldspaceGetter>();
-            */
-
-            /* ATTEMPT 2: WorldspaceContext map to cells. Cannot do this, CELLS inside Winning Worldspace Context
-             * are completely different from ACTUALLY winning cells. Contains() check will fail aftewords as it wont find the necessary CELLs.
-            var worldSpaces = state.LoadOrder.PriorityOrder.Worldspace().WinningContextOverrides();
-            worldSpaces.ForEach(w =>
-                {
-                    
-                    w.Record.EnumerateMajorRecords<ICellGetter>()
-                        .Select(c => c.FormKey)
-                        .ForEach(f =>
-                        {
-                            cellWorldspaceMap.Add(f, w);
-                        });
-                   
-                });
-            */
-
-            // ATTEMPT 3: Try direct map with worldspace search. Not possible since difficult to obtain worldspace from CellContext.
-            //<Worldspace FormKey, Worldspace|WorldspaceContext>
-            /*
-            var worldSpaces = state.LoadOrder.PriorityOrder.Worldspace().WinningContextOverrides();
-            worldSpaces.ForEach(w =>
-                {
-                    cellWorldspaceMap.Add(w.Record.FormKey, w);
-                });
-            */
-
-            /* Summary:
-             * Looped search from worldspace -> group -> subgroup cell wont work as it will give the cells contained in the winning worldspaceContext,
-             * which does not cover/contain all the winning worldspace cells, since some actually reside in losing worldspace contexts.
-             */
 
             foreach (var cellContext in state.LoadOrder.PriorityOrder.Cell().WinningContextOverrides())
             {
@@ -124,48 +69,24 @@ namespace CellEditorIDFixer
 
                     if (cell.Grid != null)
                     {
-                        //Console.WriteLine($">>> SKIPPED {cell.EditorID} as Worldspace CELL is currently unsupported.");
                         worldSpaceCellFlag = true;
                     }
 
-                    /*
-                    foreach (var groupItem in state.LoadOrder.PriorityOrder.Worldspace().WinningOverrides())
-                    {
-                        if ( groupItem.EnumerateMajorRecords<ICellGetter>().Contains(cell))
-                        {
-                            Console.WriteLine($"ALERT: Worldspace CELL currently unsupported, SKIPPED: {cell.EditorID}");
-                            worldSpaceCellFlag = true;
-                            break;
-                        }
-                    }
-                    */
-
-                    
                     if (worldSpaceCellFlag)
                     {
 
-
-                        /*
-                        if (worldSpaceFormIds.Contains(overridenCell.FormKey))
+                        if (cellWorldspaceMap.ContainsKey(cell.FormKey))
                         {
-                            worldSpaceFormIds.
-                        }*/
-
-                        var worldSpaceCell = cellContext.Record;
-
-                        if (cellWorldspaceMap.ContainsKey(worldSpaceCell.FormKey))
-                        {
-                            cellWorldspaceMap.TryGetValue(worldSpaceCell.FormKey, out var worldContext);
+                            cellWorldspaceMap.TryGetValue(cell.FormKey, out var worldContext);
                             worldContext.GetOrAddAsOverride(state.PatchMod);
-                            Console.WriteLine($"Overwrote the worldspace {worldContext.Record.Name} - {worldContext.Record.FormKey.ID} from {worldContext.ModKey.FileName} into the patch");
+                            //Console.WriteLine($"Overwrote the worldspace {worldContext.Record.Name} ID:{worldContext.Record.FormKey}" +
+                            //    $" from {worldContext.ModKey.FileName} into the patch.");
                         }
 
-                        
                         var overridenCell = cellContext.GetOrAddAsOverride(state.PatchMod);
                         overridenCell.EditorID = overridenCell.EditorID?.Replace("_", "");
                         overridenCell.Persistent.Clear();
                         overridenCell.Temporary.Clear();
-                        
                         worldCounter++;
                     }
                     else
