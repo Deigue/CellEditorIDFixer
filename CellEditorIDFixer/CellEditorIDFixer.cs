16db8366d17de5b52e1170676c15957ef77a9153
Deigue <deigue@hotmail.com>
2020-12-06 05:30:23 -0500

    Cleaned up code for more finalized release ver.

--- CellEditorIDFixer/CellEditorIDFixer.cs
+++ CellEditorIDFixer/CellEditorIDFixer.cs
@@ -1,16 +1,11 @@
 ﻿using Mutagen.Bethesda;
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Synthesis;
-using Noggog;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 
 namespace CellEditorIDFixer
 {
-    public class CellEditorIDFixer
+    public static class CellEditorIdFixer
     {
         
         public static int Main(string[] args)
@@ -30,48 +25,20 @@ namespace CellEditorIDFixer
             );
         }
 
-        public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
+        private static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
         {
             Console.WriteLine($"Running Cell Editor ID Fixer ...");
 
-            int counter = 0;
-
-
-            /*
-            //int worldCounter = 0;
-            var worldContexts = state.LoadOrder.PriorityOrder.Worldspace().WinningContextOverrides(state.LinkCache);
-            var cellWorldspaceMap = new Dictionary<FormKey, ModContext<ISkyrimMod, IWorldspace, IWorldspaceGetter>>();
-
-            // Build Map to provide path to the winning worldspace context.
-            state.LoadOrder.PriorityOrder
-                .ForEach(mod =>
-                    {
-                        mod.Mod?.Worldspaces.RecordCache.Items
-                            .ForEach(world =>
-                                {
-                                    var winningContext = worldContexts
-                                        .Where(wc => wc.Record.FormKey.Equals(world.FormKey))
-                                        .First();
-
-                                    world.EnumerateMajorRecords<ICellGetter>()
-                                        .Select(c => c.FormKey)
-                                        .ForEach(cellKey => cellWorldspaceMap.TryAdd(cellKey, winningContext));
-                                }
-                            );
-                    }
-                );
-            */
+            var counter = 0;
 
             foreach (var cellContext in state.LoadOrder.PriorityOrder.Cell().WinningContextOverrides(state.LinkCache))
             {
                 var cell = cellContext.Record;
-                if ((cell.EditorID?.Contains("_") ?? false))
-                {
-                    Console.WriteLine($"Cell EDID {cell.EditorID} in {cell.FormKey.ModKey.FileName}");
-                    var overridenCell = cellContext.GetOrAddAsOverride(state.PatchMod);
-                    overridenCell.EditorID = overridenCell.EditorID?.Replace("_", "");
-                    counter++;
-                }
+                if ((!(cell.EditorID?.Contains("_") ?? false))) continue;
+                Console.WriteLine($"Cell EDID {cell.EditorID} in {cell.FormKey.ModKey.FileName}");
+                var overridenCell = cellContext.GetOrAddAsOverride(state.PatchMod);
+                overridenCell.EditorID = overridenCell.EditorID?.Replace("_", "");
+                counter++;
             }
 
             Console.WriteLine();
